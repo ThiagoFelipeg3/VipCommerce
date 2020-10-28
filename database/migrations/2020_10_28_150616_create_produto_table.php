727318e1ca42a425f4dbd1d87172eb251131af50
Thiago Felipe <thiagof.g3@gmail.com>
2020-10-28 12:15:32 -0300

    Criando migrate para tabela produto

---
+++ database/migrations/2020_10_28_150616_create_produto_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto', function (Blueprint $table) {
+            $table->increments('codigo_produto');
+            $table->string('nome', 50)->nullable(false);
+            $table->string('cor', 20);
+            $table->decimal('tamanho', 10, 2);
+            $table->decimal('valor', 9, 2)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto');
+    }
+}
