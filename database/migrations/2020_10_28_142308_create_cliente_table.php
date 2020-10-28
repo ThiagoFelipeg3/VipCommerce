ce75fda479ec74a58b17112dc3ef04a53142385a
Thiago Felipe <thiagof.g3@gmail.com>
2020-10-28 12:05:53 -0300

    Criando migrate para tabela cliente

--- database/migrations/2020_10_28_142308_create_cliente_table.php
+++ database/migrations/2020_10_28_142308_create_cliente_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateClienteTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->increments('codigo_cliente');
+            $table->string('nome', 50);
+            $table->string('cpf', 14);
+            $table->enum('sexo', ['M', 'F']);
+            $table->string('email', 191);
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cliente');
     }
 }
